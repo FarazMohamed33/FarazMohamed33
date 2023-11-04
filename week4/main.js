f5d23dd17e937bfa0d5d8c0a082e8b2a4d6c308f
FarazMohamed33 <145095514+FarazMohamed33@users.noreply.github.com>
2023-11-05 02:30:54 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,28 +1,45 @@
-var myCity = {
-"name": "Dubai",
-"country": "UAE",
-"place": "Dubai Mall"
-};
+// Step 1: Create variables to store references to elements
+const cityInput = document.getElementById("cityInput");
+const weatherInfo = document.getElementById("weather-info");
+const btn = document.getElementById("btn");
 
-console.log(myCity.name); // Outputs: Dubai
-console.log(myCity.country); // Outputs: UAE
+// Step 2: Add an event listener to the button
+btn.addEventListener("click", () => {
+    // Step 3: Get the value of the input field (city name)
+    const cityName = cityInput.value;
 
-var myCity = ["Dubai", "UAE", "Dubai Mall"];
+    // Step 4: Check if the city input is empty
+    if (cityName === "") {
+        alert("Please enter a city name.");
+    } else {
+        // Step 5: Make an HTTP request to OpenWeatherMap API
+        const apiKey = "YOUR_API_KEY"; // Replace with your actual API key
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-console.log(myCity[0]); // Outputs: Dubai
+        fetch(apiUrl)
+            .then((response) => {
+                // Step 6: Implement error handling
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                // Update the weather info container with weather details
+                const weatherDescription = data.weather[0].description;
+                const mainTemperature = data.main.temp;
+                const windSpeed = data.wind.speed;
 
-var myCities = [
-{
-"name": "Dubai", 
-"country": "UAE", 
-"place": "Dubai Mall"
-},
-{
-"name": "Milan", 
-"country": "Italy", 
-"place": "Duomo Di Milano"
-}
-];
-
-console.log(myCities[1].country); // Outputs: Italy
-
+                const weatherHTML = `
+                    <p>Weather: ${weatherDescription}</p>
+                    <p>Temperature: ${mainTemperature} K</p>
+                    <p>Wind Speed: ${windSpeed} m/s</p>
+                `;
+                weatherInfo.innerHTML = weatherHTML;
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                alert("An error occurred while fetching weather data.");
+            });
+    }
+});
